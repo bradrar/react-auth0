506f4d618346abfd9338c44b5b91fa68e95673b6
Brad Baguyo <40604026+bradrar@users.noreply.github.com>
2020-05-04 20:40:20 +0800

    add build folder

---
+++ build/precache-manifest.9ed9cb8275705c62da1977f04cbba6e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80f2c49354a4dbf1ba67a4c24a0b8a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29707184cdf2301d4bc7",
+    "url": "/static/css/2.fbc4b4ff.chunk.css"
+  },
+  {
+    "revision": "6654d012dd2892e436d3",
+    "url": "/static/css/main.35917861.chunk.css"
+  },
+  {
+    "revision": "29707184cdf2301d4bc7",
+    "url": "/static/js/2.bf2f5826.chunk.js"
+  },
+  {
+    "revision": "596e4823497e45004d2db4c50beba3f3",
+    "url": "/static/js/2.bf2f5826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6654d012dd2892e436d3",
+    "url": "/static/js/main.f4a4a08c.chunk.js"
+  },
+  {
+    "revision": "6f8b8595a5d98ae13e6b",
+    "url": "/static/js/runtime-main.d1161680.js"
+  }
+]);
